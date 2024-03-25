d3408be40f1bc8c3bef281cd5179ecfb2b304339
Samuel Monte <montesamuelll@gmail.com>
2024-03-24 23:23:31 -0300

    Mudança xp

--- app/components/projetos/projetos.tsx
+++ app/components/projetos/projetos.tsx
@@ -6,8 +6,9 @@ export default function Projetos() {
         <div className="containerProjects ">
             <h1 className="text-4xl">Projetos</h1>
             <div className="projectDiv">
-                <a href="https://github.com/SamuelGMonte/Kabum-Searcher" className="project project1"><img src={bookmark.src} width={30} alt="" /><span>Kabum Searcher</span><p>Um simples webscrapper feito em Python</p></a>
-                <a href="https://github.com/SamuelGMonte/portfolio2.0" className="project project2"><img src={bookmark.src} width={30} alt="" /><span>Portfólio 2.0</span><p>Meu antigo portfólio</p></a>
+                <a href="https://github.com/SamuelGMonte/Kabum-Searcher" className="project"><img src={bookmark.src} width={30} alt="" /><span>Kabum Searcher</span><p>Um simples webscrapper feito em Python</p></a>
+                <a href="https://github.com/SamuelGMonte/portfolio2.0" className="project"><img src={bookmark.src} width={30} alt="" /><span>Portfólio 2.0</span><p>Meu antigo portfólio</p></a>
+                <a href="https://github.com/SamuelGMonte/sptransjava" className="project"><img src={bookmark.src} width={30} alt="" /><span>BuSearcher</span><p>Aplicativo de consumo da API da SPTrans para a atualização de veículos em tempo real feito em Java</p></a>
             </div>
         </div>
 
--- app/components/timeline/timeline.tsx
+++ app/components/timeline/timeline.tsx
@@ -4,30 +4,41 @@ export default function Timeline() {
 return (
 <section className="design-section">
     <div className="timeline">
-        <div className="timeline-empty">
+
+        <div className="timeline-content">
+            <h3>Judge.me</h3>
+            <span>2024 - Atual</span>
+            <p>Judge.me é uma empresa que fornece soluções de avaliação e análise de produtos para lojas online e e-commerce. Oferecendo um serviço de revisões de produtos que os comerciantes podem integrar em seus sites para coletar e exibir comentários de clientes sobre os produtos que vendem. Essas revisões podem ajudar os consumidores a tomar decisões de compra informadas e também podem ajudar as empresas a melhorar a qualidade de seus produtos e serviços com base no feedback dos clientes.</p>
+            <p>Competências: Atendimento ao cliente · Comunicação em geral · Instalações do App em sites Shopify e WordPress. · Rich Snippets </p>
+            <p>Customer Support Agent</p>
+
         </div>
 
-        <div className="timeline-middle">
-            <div className="timeline-circle"></div>
-        </div>
-        <div className="timeline-component timeline-content">
-        <h3>Flexdoc</h3>
-        <span>2023 - Atual</span>
+        <div className="timeline-middle"><div className="timeline-circle"></div></div>
+
+        <div className="timeline-empty"></div>
+
+        <div className="timeline-empty"></div>
+
+        <div className="timeline-middle"><div className="timeline-circle"></div></div>
+
+        <div className="timeline-content">
+            <h3>Flexdoc</h3>
+            <span>2023 - 2024</span>
             <p>A Flexdoc é uma empresa sediada em Brasília/DF, com atuação nacional, focada no desenvolvimento de soluções inovadoras e no fornecimento de serviços técnicos especializados, direcionados para a automação de fluxos de negócios, processamento de dados, e gestão documental, atuando desde a instalação e implantação das tecnologias até produção, digitalização, extração, tramitação, controle e disponibilização da informação com tecnologias no estado-da-arte.</p>
             <p>Competências: Auxílio na implantação e Gestão de Ambientes Windows Server, ambientes Linux, auxílio ao usuário, realização de manutenções preventivas, acompanhamento de chamados e documentação de projetos</p>
             <p>Cargo: Estagiário</p>
         </div>
-        <div className="timeline-component timeline-content">
-        <h3>Lew’Lara\TBWA </h3>
-        <span>2022</span>
+        
+        <div className="timeline-content">
+            <h3>Lew’Lara\TBWA </h3>
+            <span>2022 - 2023</span>
             <p>Fundada em 1992 e desde 2007 parte do grupo TBWA é uma das agências de comunicação mais reconhecidas do Brasil.</p>
             <p>Competências: Atendimento ao cliente · Solução de problemas de hardware · Suporte de hardware de TI</p>
             <p>Cargo: Estagiário</p>
         </div>
-        <div className="timeline-middle">
-            <div className="timeline-circle"></div>
-        </div>
-        
+
+        <div className="timeline-middle"><div className="timeline-circle"></div></div>
 
     </div>
 </section>
